ead7033bb67e10688a92168b2411bebc8e837458
Masatoshi Furukawa <furukawa@fullness.co.jp>
2025-11-16 10:56:50 +0900

    演習-06

---
+++ Infrastructures/Accessors/DepartmentAccessor.cs
@@ -0,0 +1,53 @@
+﻿using CS_DB_Exercise_Answer.Infrastructures.Contexts;
+using CS_DB_Exercise_Answer.Infrastructures.Entities;
+
+namespace CS_DB_Exercise_Answer.Infrastructures.Accessors;
+
+/// <summary>
+/// departmentテーブルにアクセスするクラス
+/// </summary>
+public class DepartmentAccessor
+{
+    private readonly AppDbContext _context;
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="context">演習用DbContext</param>
+    public DepartmentAccessor(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    /// <summary>
+    /// 演習-05 departmentテーブルからすべての部署を取得する
+    /// </summary>
+    /// <returns>取得結果</returns>
+    public List<DepartmentEntity> FindAll()
+    {
+        return _context.Departments.ToList();
+    }
+
+    /// <summary>
+    /// 演習-06 departmentテーブルから主キー値で部署を取得する
+    /// </summary>
+    /// <returns>取得結果</returns>
+    /*
+    public DepartmentEntity? FindById(int id)
+    {
+        var department = _context.Departments.Find(id);
+        return department;
+    }
+    */
+    
+    /// <summary>
+    /// 演習-06 departmentテーブルから主キー値で部署を取得する
+    /// </summary>
+    /// <returns>取得結果</returns>
+    public DepartmentEntity? FindById(int id)
+    {
+        var department = _context.Departments
+            .Where(d => d.Id == id)
+            .SingleOrDefault();
+        return department;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
-﻿namespace CS_DB_Exercise_Answer;
+﻿using CS_DB_Exercise_Answer.Infrastructures.Accessors;
+using CS_DB_Exercise_Answer.Infrastructures.Contexts;
 
+namespace CS_DB_Exercise_Answer;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+         // 演習用DbContextを生成する
+        var context = new AppDbContext();
+        // departmentテーブルアクセスクラスを生成する
+        var accessor = new DepartmentAccessor(context);
+
+        Console.Write("部署Idを入力してください->");
+        var deptId = int.Parse(Console.ReadLine()!);
+        var department = accessor.FindById(deptId);
+        Console.WriteLine("演習-06 departmentテーブルから主キー値で部署を取得する");
+        if (department != null)
+        {
+            Console.WriteLine(department);
+        }
+        else
+        {
+            Console.WriteLine($"部署Id:{deptId}の部署は存在しません");
+        }
     }
 }
